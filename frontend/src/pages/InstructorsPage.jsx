2388bff3946c6eb1e725bae2b2244efbd998163e
Chaeson <only9984@gmail.com>
2025-11-23 23:01:33 +0900

    호출부 로그 확인

--- frontend/src/pages/InstructorsPage.jsx
+++ frontend/src/pages/InstructorsPage.jsx
@@ -124,7 +124,7 @@ const InstructorsPage = () => {
     const fetchInstructors = async () => {
       try {
         setLoading(true);
-        const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+        const apiUrl = 'http://158.180.83.230:8080';
         console.log(apiUrl)
         const response = await axios.get(`${apiUrl}/api/instructors`);
         const data = response.data;
